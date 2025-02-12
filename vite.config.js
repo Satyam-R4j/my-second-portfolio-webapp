db3ed5e0a9a41f4c0151931d7f17308e0517be18
Satyam-R4j <satyamraj418@gmail.com>
2025-02-13 00:46:43 +0530

    new commit

--- vite.config.js
+++ vite.config.js
@@ -8,16 +8,33 @@
 // })
 
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   build: {
+//     rollupOptions: {
+//       external: ['react', 'react-dom'] // Add external dependencies here
+//     }
+//   }
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react', 'react-dom'] // Add external dependencies here
+      external: ['react', 'react-dom']
+    }
+  },
+  resolve: {
+    alias: {
+      react: "react",
+      "react-dom": "react-dom"
     }
   }
-})
+});
